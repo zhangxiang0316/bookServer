440a2749de8960dcb36a14850f6959f61ce752f8
zhangxiang <zhangxiang@everisetech.com>
2021-12-07 17:30:40 +0800

    Initial commit

---
+++ db/mysql.js
@@ -0,0 +1,40 @@
+/**
+ * create by zhangxiang on 2021-12-05 12:31
+ * 类注释：
+ * 备注：
+ */
+
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+    host: '150.158.52.240',
+    port: 3306,
+    user: 'root',
+    password: '123456',
+    database: 'book'
+})
+
+con.connect(err => {
+    if (err) {
+        console.log('链接失败', err)
+    } else {
+        console.log('链接成功')
+    }
+})
+
+const execute = (sql) => {
+    return new Promise((resolve, reject) => {
+        con.query(sql, (err, result) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(JSON.parse(JSON.stringify(result))[0])
+        })
+    })
+}
+
+module.exports = {
+    execute
+}
+
